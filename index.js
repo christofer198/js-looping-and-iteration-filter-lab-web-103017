06a543d828fda1e94162cff16f90aa1b10f702fd
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-12-12 00:54:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file
+function findMatching(drivers, name){
+  const newArray = [];
+  for(i = 0; i < drivers.length; i++){
+    if(drivers[i].toUpperCase() == name.toUpperCase()){
+      newArray.push(drivers[i]);
+    }
+  }
+  return newArray;
+}
+
+function fuzzyMatch(){
+
+}
